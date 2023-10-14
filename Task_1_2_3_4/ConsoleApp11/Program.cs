beb3730ce60fb504f64065876bc35179c835de15
unknown <fuadgi@code.edu.az>
2023-10-14 14:55:18 +0400

    Addedd

---
+++ Task_1_2_3_4/ConsoleApp11/Program.cs
@@ -0,0 +1,81 @@
+﻿#region task1
+//int num1 = 7;
+//int num2 = 36;
+//int icreasment = 0;
+//for (int i = num1; i < num2; i++)
+//{
+//    if (num1 < num2)
+//    {
+//        if (i % 2 == 1)
+//        {
+//            icreasment++;
+//            //Console.WriteLine(i);
+//        }
+//    }
+//}
+//Console.WriteLine(icreasment);
+#endregion
+#region task2
+//int num3 = 13;
+//if (num3 == 1)
+//{
+//    Console.WriteLine($"{num3} bu sade ve murekkebdir");
+//}
+//else if (num3 == 2 || num3 == 3)
+//{
+//    Console.WriteLine($"{num3} sade ededir");
+//}
+//else if (num3 % 2 == 0 || num3 % 3 == 0)
+//{
+//    Console.WriteLine($"{num3} murekkeb ededdir");
+//}
+//else
+//{
+//    Console.WriteLine($"{num3} sade ededdir..");
+//}
+#endregion
+#region task3
+//int num5 = 32;
+//bool isPowerOf2 = true; // bunu 2 nin quvveti kimi goturek
+
+//if (num5 <= 0)
+//{
+//    Console.WriteLine($"{num5} 2nin quvveti deyil"); //  0 ve menfi ededler 2nin quvveti ola bilmez
+//}
+//else
+//{
+//    while (num5 > 1)
+//    {
+//        if (num5 % 2 != 0)
+//        {
+//            isPowerOf2 = false;
+
+//        }
+//        break;
+//    }
+
+//    if (isPowerOf2)
+//    {
+//        Console.WriteLine($"{num5} is a power of 2");
+//    }
+//    else
+//    {
+//        Console.WriteLine($"{num5} is not a power of 2");
+//    }
+//}
+#endregion
+#region task4
+//int num = 145;
+//int result = 0;
+//while (num > 0)//burda while dongu 0-dan böyük olana kimi
+//{
+//    result++;//burda biz ededi ona bolduyumuz qeder artacaq ve burda 3defe 10 bolunub ona gore cavab 3 olur
+//    num /= 10;
+//}
+//Console.WriteLine(result);
+#endregion
+string name = "Fuad";
+char firstletters = 'A';
+string adress = "bileceri";
+int age = 1;
+Console.WriteLine("1");
